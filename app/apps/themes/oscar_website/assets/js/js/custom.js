50de357c06802d8a9012e24a9e60176c2740b418
gechleng <gechleng@rotati.com>
2017-12-01 12:23:58 +0700

    fix home concept animation

--- app/apps/themes/oscar_website/assets/js/js/custom.js
+++ app/apps/themes/oscar_website/assets/js/js/custom.js
@@ -1,1 +1,38 @@
+$(document).ready(function(){
+    /*
+      Circle Slider
+    */
+    if ($.isFunction($.fn.flipshow)) {
+      var circleContainer = $('#fcSlideshow');
+
+      if (circleContainer.get(0)) {
+        circleContainer.flipshow();
+
+        setTimeout(function circleFlip() {
+          circleContainer.data().flipshow._navigate(circleContainer.find('div.fc-right span:first'), 'right');
+          setTimeout(circleFlip, 3000);
+        }, 3000);
+      }
+    }
+
+    /*
+    Move Cloud
+    */
+    if ($('.cloud').get(0)) {
+      var moveCloud = function() {
+        $('.cloud').animate({
+          'top': '+=20px'
+        }, 3000, 'linear', function() {
+          $('.cloud').animate({
+            'top': '-=20px'
+          }, 3000, 'linear', function() {
+            moveCloud();
+          });
+        });
+      };
+
+      moveCloud();
+    }
+
+});
 
