23e961a3050d547c4d7fc933d8eafa92a49313e1
gechleng <gechleng@rotati.com>
2017-12-12 15:48:51 +0700

    Update about page (#13)
    * change style hearder testimonial font size
    
    * fix long text over layout and contact button
    
    * fix about us show error 500
    
    * change style pricing of sign up button
    
    * fix text slider run smooth
    
    * add contact send email form to conact page
    
    * add * to subject of email from
    
    * improve home page, change footer logo link to home page, take out action from contact email
    
    * fix footer logo
    
    * cut down white space form feature title and fix bottom page white space
    
    * config send email
    
    * remove sitemap, hide the blue counter bar, remove testimonial, remove blue bar chart, remove footer about page
    
    * move js mail to bottom of file
    
    * get comment email to js

--- app/apps/themes/oscar_website/assets/js/js/custom.js
+++ app/apps/themes/oscar_website/assets/js/js/custom.js
@@ -1,47 +1,4 @@
 $(document).ready(function(){
-  // mail
-
-  $form = $('#contactForm');
-  return $form.submit(function() {
-    var formData, email, message, name, subject;
-    name = $('input#name').val();
-    subject = $('input#subject').val();
-    email = $('input#email').val();
-    message = $('textarea#message').val();
-    if(name != "" || subject != "" || email != "" || message != ""){
-      $('#sendMessage').attr('disabled','disabled');
-      $('#sendMessage').val('Sending...')
-    }
-
-    formData = {
-      name: name,
-      email: email,
-      subject: subject,
-      message: message
-    };
-
-    $.ajax({
-      type: 'POST',
-      dataType: 'json',
-      url: $form.attr('action'),
-      data: formData,
-      success: function() {
-        $form[0].reset();
-        $('#sendMessage').removeAttr('disabled');
-        $('#sendMessage').val('Send Message')
-        alert('Your message has been sent successfully. Thank you.');
-        return false;
-      },
-      error: function() {
-        $('#sendMessage').removeAttr('disabled');
-        $('#sendMessage').val('Send Message')
-        alert('Your message has not been sent. Please try again later!');
-        return false;
-      }
-    });
-    return false;
-  });
-
   $("#js-rotating-home").Morphext({
     // The [in] animation type. Refer to Animate.css for a list of available animations.
     animation: "fadeIn",
@@ -116,92 +73,92 @@ $(document).ready(function(){
 
     (function(theme, $) {
 
-    theme = theme || {};
+      theme = theme || {};
 
-    var instanceName = '__progressBar';
+      var instanceName = '__progressBar';
 
-    var PluginProgressBar = function($el, opts) {
-      return this.initialize($el, opts);
-    };
+      var PluginProgressBar = function($el, opts) {
+        return this.initialize($el, opts);
+      };
 
-    PluginProgressBar.defaults = {
-      accX: 0,
-      accY: -50,
-      delay: 1
-    };
+      PluginProgressBar.defaults = {
+        accX: 0,
+        accY: -50,
+        delay: 1
+      };
 
-    PluginProgressBar.prototype = {
-      initialize: function($el, opts) {
-        if ($el.data(instanceName)) {
+      PluginProgressBar.prototype = {
+        initialize: function($el, opts) {
+          if ($el.data(instanceName)) {
+            return this;
+          }
+
+          this.$el = $el;
+
+          this
+            .setData()
+            .setOptions(opts)
+            .build();
+
+          return this;
+        },
+
+        setData: function() {
+          this.$el.data(instanceName, this);
+
+          return this;
+        },
+
+        setOptions: function(opts) {
+          this.options = $.extend(true, {}, PluginProgressBar.defaults, opts, {
+            wrapper: this.$el
+          });
+
+          return this;
+        },
+
+        build: function() {
+          if (!($.isFunction($.fn.appear))) {
+            return this;
+          }
+
+          var self = this,
+            $el = this.options.wrapper,
+            delay = 1;
+
+          $el.appear(function() {
+
+            delay = ($el.attr('data-appear-animation-delay') ? $el.attr('data-appear-animation-delay') : self.options.delay);
+
+            $el.addClass($el.attr('data-appear-animation'));
+
+            setTimeout(function() {
+
+              $el.animate({
+                width: $el.attr('data-appear-progress-animation')
+              }, 1500, 'easeOutQuad', function() {
+                $el.find('.progress-bar-tooltip').animate({
+                  opacity: 1
+                }, 500, 'easeOutQuad');
+              });
+
+            }, delay);
+
+          }, {
+            accX: self.options.accX,
+            accY: self.options.accY
+          });
+
           return this;
         }
-
-        this.$el = $el;
-
-        this
-          .setData()
-          .setOptions(opts)
-          .build();
-
-        return this;
-      },
-
-      setData: function() {
-        this.$el.data(instanceName, this);
-
-        return this;
-      },
-
-      setOptions: function(opts) {
-        this.options = $.extend(true, {}, PluginProgressBar.defaults, opts, {
-          wrapper: this.$el
-        });
-
-        return this;
-      },
-
-      build: function() {
-        if (!($.isFunction($.fn.appear))) {
-          return this;
-        }
-
-        var self = this,
-          $el = this.options.wrapper,
-          delay = 1;
-
-        $el.appear(function() {
-
-          delay = ($el.attr('data-appear-animation-delay') ? $el.attr('data-appear-animation-delay') : self.options.delay);
-
-          $el.addClass($el.attr('data-appear-animation'));
-
-          setTimeout(function() {
-
-            $el.animate({
-              width: $el.attr('data-appear-progress-animation')
-            }, 1500, 'easeOutQuad', function() {
-              $el.find('.progress-bar-tooltip').animate({
-                opacity: 1
-              }, 500, 'easeOutQuad');
-            });
-
-          }, delay);
-
-        }, {
-          accX: self.options.accX,
-          accY: self.options.accY
-        });
-
-        return this;
-      }
-    };
+      };
 
     // expose to scope
     $.extend(theme, {
       PluginProgressBar: PluginProgressBar
     });
 
-    // jquery plugin
+      // jquery plugin
     $.fn.themePluginProgressBar = function(opts) {
       return this.map(function() {
         var $this = $(this);
@@ -214,8 +171,51 @@ $(document).ready(function(){
 
       });
     }
-
   }).apply(this, [window.theme, jQuery]);
+
+  // mail
+
+  $form = $('#contactForm');
+  return $form.submit(function() {
+    var formData, email, message, name, subject;
+    name = $('input#name').val();
+    subject = $('input#subject').val();
+    email = $('input#email').val();
+    message = $('textarea#message').val();
+    if(name != "" || subject != "" || email != "" || message != ""){
+      $('#sendMessage').attr('disabled','disabled');
+      $('#sendMessage').val('Sending...')
+    }
+
+    formData = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message
+    };
+
+    $.ajax({
+      type: 'POST',
+      dataType: 'json',
+      url: $form.attr('action'),
+      data: formData,
+      success: function() {
+        $form[0].reset();
+        $('#sendMessage').removeAttr('disabled');
+        $('#sendMessage').val('Send Message')
+        alert('Your message has been sent successfully. Thank you.');
+        return false;
+      },
+      error: function() {
+        $('#sendMessage').removeAttr('disabled');
+        $('#sendMessage').val('Send Message')
+        alert('Your message has not been sent. Please try again later!');
+        return false;
+      }
+    });
+    return false;
+  });
+
 
 });
 
